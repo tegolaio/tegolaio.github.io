5f1c44f48621660e1c95235948e9770ae2d86cf9
Matteo <github@mattheory.it>
2025-01-29 19:32:07 +0100

    Gallery v3 update

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const gallery = document.querySelector('.gallery');
+    const images = Array.from(gallery.children);
     let isDragging = false;
     let startX, scrollLeft;
     let autoScroll;
@@ -21,26 +22,27 @@ document.addEventListener('DOMContentLoaded', function () {
         stopAutoScroll();
     });
 
-gallery.addEventListener('touchmove', (e) => {
-    if (!isDragging) return;
-    e.preventDefault(); // Previene lo scroll verticale interferente
-    const x = e.touches[0].pageX - gallery.offsetLeft;
-    const walk = (x - startX) * 1.5;
-    gallery.scrollLeft = scrollLeft - walk;
-}, { passive: false }); // Imposta passive a false per permettere preventDefault()
+    gallery.addEventListener('touchmove', (e) => {
+        if (!isDragging) return;
+        e.preventDefault(); // Previene lo scroll verticale interferente
+        const x = e.touches[0].pageX - gallery.offsetLeft;
+        const walk = (x - startX) * 1.5;
+        gallery.scrollLeft = scrollLeft - walk;
+    }, { passive: false }); // Imposta passive a false per permettere preventDefault()
 
     // Stop scorrimento con cursore su PC
     gallery.addEventListener("mouseover", function () {
     gallery.style.animationPlayState = "paused"; // Ferma l'animazione
     });
-
+        
     gallery.addEventListener("mouseleave", function () {
     gallery.style.animationPlayState = "running"; // Riprende l'animazione
     });
-
-    gallery.addEventListener('touchend', () => {
-        isDragging = false;
-        setTimeout(startAutoScroll, 2000); // Riprende lo scorrimento dopo 2 secondi
+        
+    // Duplica le immagini per rendere il loop senza scatti
+    images.forEach(image => {
+        let clone = image.cloneNode(true);
+        gallery.appendChild(clone);
     });
 
     startAutoScroll();
